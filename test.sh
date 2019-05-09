4c7fe616e8a489bdd8c8108bf80084f9002c5683
darkdread <sakatagintoki9001@gmail.com>
2019-05-09 17:29:41 +0800

    new shit

--- test.sh
+++ test.sh
@@ -49,22 +49,26 @@ else
     if [ ! -d "$dir" ]; then
         echo "The home directory ($dir) of user $user does not exist."
     else
-            dirperm=`ls -ld $dir | cut -f1 -d" "`
+        dirperm=`ls -ld $dir | cut -f1 -d" "`
         if [ `echo $dirperm | cut -c6` != "-" ]; then
             echo "Group Write permission set on the home directory ($dir) of user
             $user"
+            chmod g-w "$dir"
         fi
         if [ `echo $dirperm | cut -c8` != "-" ]; then
             echo "Other Read permission set on the home directory ($dir) of user
             $user"
+            chmod o-r "$dir"
         fi
         if [ `echo $dirperm | cut -c9` != "-" ]; then
             echo "Other Write permission set on the home directory ($dir) of user
             $user"
+            chmod o-w "$dir"
         fi
         if [ `echo $dirperm | cut -c10` != "-" ]; then
             echo "Other Execute permission set on the home directory ($dir) of user
             $user"
+            chmod o-x "$dir"
         fi
     fi
     done

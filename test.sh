49f6c223a17c46a6bf1badeaa85ee3fdc765598f
darkdread <sakatagintoki9001@gmail.com>
2019-06-10 18:08:06 +0800

    new things

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # Today's date
-$today = date --date
+$today = date --date ""
 
 # Get all users' last password change date
-awk -F: '{print $1}' /etc/passwd | xargs -I {} chage --list {} | grep "Last password change" | awk -F: '{print $2'} | xargs -I {} date --date "{}"
+$ALL_USERS_LAST_CHANGE_DATE=$(awk -F: '{print $1}' /etc/passwd | xargs -I {} chage --list {} | grep "Last password change" | awk -F: '{print $2'} | xargs -I {} date --date "{}")
 
 echo "Today: $today"

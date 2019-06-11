fcbcfa0be9cd2a3d875bb11059b3a48873ac25fd
darkdread <sakatagintoki9001@gmail.com>
2019-06-11 10:10:19 +0800

    new things

--- test.sh
+++ test.sh
@@ -2,18 +2,22 @@
 today=$(date --date "")
 
 # Get all users' last password change date
-ALL_USERS_LAST_CHANGE_DATE=$(awk -F: '{print $1}' /etc/passwd | xargs -I {} chage --list {} | grep "Last password change" | awk -F: '{print $2'} | xargs -I {} date --date "{}" | xargs -I {} echo "{}\n")
+ALL_USERS_LAST_CHANGE_DATE=$(awk -F: '{print $1}' /etc/passwd | xargs -I {} chage --list {} | grep "Last password change" | awk -F: '{print $2'} | xargs -I {} date --date "{}" | xargs -I {} echo "{}, ")
 
-SAVEIFS=$IFS
-IFS=$'\n'
-ARR_DATES=$($ALL_USERS_LAST_CHANGE_DATE)
-IFS=$SAVEIFS
+str=$ALL_USERS_LAST_CHANGE_DATE
+
+arr_dates=()
+while [[ $str =~ ([^,]+)(,[ ]+|$) ]]; do
+    arr_dates+=("${BASH_REMATCH[1]}")   # capture the field
+    i=${#BASH_REMATCH}              # length of field + delimiter
+    str=${str:i}                    # advance the string by that length
+done                                # the loop deletes $str, so make a copy if needed
 
 echo "Today: $today"
 echo $ALL_USERS_LAST_CHANGE_DATE
-echo $ARR_DATES
+echo $arr_dates
 
-for change_date in "${ALL_USERS_LAST_CHANGE_DATE[@]}"
+for change_date in "${arr_dates[@]}"
 do
     echo $change_date
 done
